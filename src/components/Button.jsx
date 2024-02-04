c701a39bb7a3d48a3d149f494c290f3baabe9b49
VanyaMark <markova.vanya@gmail.com>
2024-02-04 22:09:15 +0100

    Created functions to start the tone on mouse down and to stop it with some delay on mouse up

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,11 @@ import * as Tone from 'tone';
 
 function Button() {
     const handleTone = () => {
-        const synth = new Tone.Synth();
+        const synth = new Tone.FMSynth();
         const feedbackDelay = new Tone.FeedbackDelay();
         synth.connect(feedbackDelay)
         feedbackDelay.toDestination();
-        synth.triggerAttackRelease("C4", "8n")
+        synth.triggerAttackRelease("E3", "8n")
     }
     return (
         <button className="btn" onClick={handleTone}>Check Here!</button>
--- src/components/GuitarTone.jsx
+++ src/components/GuitarTone.jsx
@@ -3,33 +3,30 @@ import * as Tone from "tone";
 
 const GuitarTone = () => {
   const [isMouseDown, setIsMouseDown] = useState(false);
-  const now = Tone.now();
+  
+  // Create a FMSynth for a guitar-like sound
+  const synth = new Tone.FMSynth().toDestination();
 
   const handleMouseDown = () => {
-    setIsMouseDown(true);
 
-    // Create a PluckSynth for a guitar-like sound
-    const synth = new Tone.PluckSynth().toDestination();
+
 
     // Trigger attack when mouse is down
-    synth.triggerAttack("E4");
+    synth.triggerAttack("C2");
 
   };
 
   const handleMouseUp = () => {
-    setIsMouseDown(false);
-
-    // Release the note when mouse is up
-    Tone.Transport.scheduleOnce(() => {
-      const synth = new Tone.PluckSynth().toDestination();
-      synth.triggerRelease();
-    }, '+0.1'); // Add a small delay to release after mouse up
+        // Use setTimeout to ensure the triggerRelease is called after a short delay
+        setTimeout(() => {
+            synth.triggerRelease();
+          }, 100); // Adjust the delay as needed
   };
 
   return (
     <div>
       <h1>Guitar Player</h1>
-      <button onClick={handleMouseDown} onMouseUp={handleMouseUp}>
+      <button onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
         Play Guitar Sound
       </button>
     </div>
