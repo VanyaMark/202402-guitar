6bd536518aba22b55bb966d14133d619bdc5e929
VanyaMark <markova.vanya@gmail.com>
2024-02-09 22:34:05 +0100

    add til Fret 12, program works before styling with react bootstrap

--- src/App.js
+++ src/App.js
@@ -15,6 +15,16 @@ function App() {
         />
         <FretContainer title="Fret 1" notes="F2 A#2 D#3 G#3 C4 F4" />
         <FretContainer title="Fret 2" notes="F#2 B2 E3 A3 C#4 F#4" />
+        <FretContainer title="Fret 3" notes="G2 C3 F3 A#3 D4 G4" />
+        <FretContainer title="Fret 4" notes="G#2 C#3 F#3 B3 D#4 G#4" />
+        <FretContainer title="Fret 5" notes="A2 D3 G3 C4 E4 A4" />
+        <FretContainer title="Fret 6" notes="A#2 D#3 G#3 C#4 F4 A#4" />
+        <FretContainer title="Fret 7" notes="B2 E3 A3 D4 F#4 B4" />
+        <FretContainer title="Fret 8" notes="C3 F3 A#3 D#4 G4 C5" />
+        <FretContainer title="Fret 9" notes="C#3 F#3 B3 E4 G#4 C#5" />
+        <FretContainer title="Fret 10" notes="D3 G3 C4 F4 A4 D5" />
+        <FretContainer title="Fret 11" notes="D#3 G#3 C#4 F#4 A#4 D#5" />
+        <FretContainer title="Fret 12" notes="E3 A3 D4 G4 B4 E5" />
       </div>
     </div>
   );
